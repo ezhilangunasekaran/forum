6ada06fce0661fbbad655989eb1c98f08840d35d
Ezhil <g.ezhilan1@gmail.com>
2017-03-20 00:05:07 -0500

    data from user is fetched and displayed in ui

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import Post from './post';
+import Users from  './users';
 import ui from './ui';
 
 Post.findAll()
     .then(ui.renderPosts)
     .catch((error)=> {
+        console.log(error);
+});
+
+Users.findActive()
+    .then(ui.renderUsers)
+    .catch((error)=>{
         console.log(error);
 });
--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,5 @@
+import xss from 'xss-filters';
+
 let ui ={
     renderPosts(posts){
         let elements = posts.map((post)=>{
@@ -6,18 +8,39 @@ let ui ={
         });
         let target = document.querySelector(".container");
         target.innerHTML = elements.join("");
+    },
+
+    renderUsers(users){
+        let elements = users.map((user)=>{
+            let {name,avatar} = user;
+            return userList(name,avatar);
+        });
+        let target = document.querySelector(".sidebar");
+        target.innerHTML = elements.join("");
     }
 }
  function articleTemplate(title,lastReply){
+    let safeTitle = xss.inHTMLData(title);
+    let safeLastReply = xss.inHTMLData(lastReply);
     let template =`
     <article class='post'>
     <h2 class='post-title'>
-    ${title}
+    ${safeTitle}
     </h2>
     <p class="post-meta">
-    ${lastReply}
+    ${safeLastReply}
     </p>
     </article>`;
 return template;
+}
+
+function userList(name,avatar) {
+     let safeName = xss.inHTMLData(name);
+     let safeAvatar = xss.inHTMLData(avatar);
+     let template =`<div class='user'>
+    <h3>${safeName}</h3>
+    <img src="assets/${safeAvatar}">
+    </div>`;
+    return template;
 }
 export default ui;
